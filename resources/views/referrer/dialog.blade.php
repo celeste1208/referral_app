476b985f0d872b92521c61dd11f98e8b45ce4cd0
naru <guroandnarun8@gmail.com>
2022-04-16 15:54:21 +0900

    share画面のUI作成

--- resources/views/referrer/dialog.blade.php
+++ resources/views/referrer/dialog.blade.php
@@ -5,16 +5,19 @@ const body = `
           このサービスを、より多くの方に使っていただきたいと考えています。そこで、あなたの大切な友人にご紹介いただけると嬉しいです。<br>
           あなたが紹介した方が、3ヶ月以内に本サービスで支払いを行うと、○○○より、感謝の気持ちのアマゾンギフトカードをプレゼントします。
         </p>
-        <div class="text-label">あなたのメールアドレス</div>
-        <input class="input-text" id="dialog-input" type="email" placeholder="test@example.com" autocomplete="off" required maxlength="64">
+
+        <div class="input-area">
+            <div class="text-label">あなたのメールアドレス</div>
+            <input value="hoge@co.jp" class="input-text" id="dialog-input" type="email" placeholder="test@example.com" autocomplete="off" required maxlength="64">
+        </div>
 
         <label class="checkbox-line" for="terms_policy">
           <input type="checkbox" id="terms_policy"><a href="http://127.0.0.1:8000/privacyPolicy" target="_blank" rel="noopener">プライバシーポリシー</a>と<a href="http://127.0.0.1:8000/terms" target="_blank" rel="noopener">利用規約</a>に同意する
         </label>
 
         <div class="button-area flex">
-            <button class="button--disabled button-primary" id="submit-email">紹介リンクを発行する
-        </div>
+            <button class="button button-primary" id="submit-email">紹介リンクを発行する
+          </div>
         <p class="text-small"><a href="https://relic.co.jp/" target="_blank" rel="noopener">RUFUを利用中</a></p>
 
     </div>
@@ -42,11 +45,9 @@ var form_validation = function() {
   }
 
   if(valid_form) {
-    submitButton.classList.add('button');
     submitButton.classList.remove('button--disabled');
   } else {
     submitButton.classList.add('button--disabled');
-    submitButton.classList.remove('button');
   }
 }
 
@@ -61,7 +62,6 @@ input.addEventListener('input', () => {
 
 terms_policy.addEventListener('input', () => {
     if (terms_policy.checked) {
-        console.log("222")
         valid_checkbox = true;
     } else {
       valid_checkbox = false;
